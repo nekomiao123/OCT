4d7eb16594eb85d9b8fde3a94dfb89883dad08f0
nekokiku <447140443@qq.com>
2024-06-26 19:57:10 +0800

    change to BV

--- oct.sh
+++ oct.sh
@@ -8,7 +8,6 @@ source='B' target='O' > logs/BO_$ename.log 2>&1 & \
 CUDA_VISIBLE_DEVICES='4' nohup python train.py \
 exp_name=BS_$ename num_epoch=$epochs model=$net \
 source='B' target='S'  > logs/BS_$ename.log 2>&1 & \
-CUDA_VISIBLE_DEVICES='5' nohup python train.py \
-exp_name=OV_$ename num_epoch=$epochs model=$net \
-source='O' target='V' train.mixup_rate=0.1 > logs/OV_$ename.log 2>&1 & \
-
+CUDA_VISIBLE_DEVICES='4' nohup python train.py \
+exp_name=BS_$ename num_epoch=$epochs model=$net \
+source='B' target='V'  > logs/BV_$ename.log 2>&1 & \
